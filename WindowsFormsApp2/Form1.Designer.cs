913e82d41a0b064fc111898af6eac85264147612
husye <husye@LAPTOP-94GEIJ6I>
2022-11-18 14:32:11 +0400

    Add project files.

---
+++ WindowsFormsApp2/Form1.Designer.cs
@@ -0,0 +1,94 @@
+﻿namespace WindowsFormsApp2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.timeLbl = new System.Windows.Forms.Label();
+            this.BakuBtn = new System.Windows.Forms.Button();
+            this.LondonBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // timeLbl
+            // 
+            this.timeLbl.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.timeLbl.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.timeLbl.Cursor = System.Windows.Forms.Cursors.Default;
+            this.timeLbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.timeLbl.Location = new System.Drawing.Point(300, 30);
+            this.timeLbl.Margin = new System.Windows.Forms.Padding(15, 0, 15, 0);
+            this.timeLbl.MaximumSize = new System.Drawing.Size(300, 300);
+            this.timeLbl.Name = "timeLbl";
+            this.timeLbl.Size = new System.Drawing.Size(100, 50);
+            this.timeLbl.TabIndex = 0;
+            this.timeLbl.Text = "START";
+            // 
+            // BakuBtn
+            // 
+            this.BakuBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BakuBtn.Location = new System.Drawing.Point(28, 354);
+            this.BakuBtn.Name = "BakuBtn";
+            this.BakuBtn.Size = new System.Drawing.Size(80, 80);
+            this.BakuBtn.TabIndex = 1;
+            this.BakuBtn.Text = "BAku";
+            this.BakuBtn.UseVisualStyleBackColor = true;
+            this.BakuBtn.Click += new System.EventHandler(this.BakuBtn_Click);
+            // 
+            // LondonBtn
+            // 
+            this.LondonBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LondonBtn.Location = new System.Drawing.Point(638, 354);
+            this.LondonBtn.Name = "LondonBtn";
+            this.LondonBtn.Size = new System.Drawing.Size(80, 80);
+            this.LondonBtn.TabIndex = 2;
+            this.LondonBtn.Text = "London";
+            this.LondonBtn.UseVisualStyleBackColor = true;
+            this.LondonBtn.Click += new System.EventHandler(this.LondonBtn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.LondonBtn);
+            this.Controls.Add(this.BakuBtn);
+            this.Controls.Add(this.timeLbl);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label timeLbl;
+        private System.Windows.Forms.Button BakuBtn;
+        private System.Windows.Forms.Button LondonBtn;
+    }
+}
+
---
+++ WindowsFormsApp2/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        Timer timer=new Timer();
+        public Form1()
+        {
+            InitializeComponent();
+            this.Text = "first task";
+            timer.Interval = 1000;
+            timer.Tick += Timer_Tick;
+            timer.Start();
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            timeLbl.Text=DateTime.Now.ToLongTimeString();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            PictureBox pb1 = new PictureBox();
+            pb1.ImageLocation = "https://i.pinimg.com/originals/51/9d/47/519d4750ebfd7df5fa7788fb0850621e.jpg";
+            pb1.SizeMode = PictureBoxSizeMode.AutoSize;
+            this.Controls.Add(pb1);
+            
+        }
+
+
+        private void LondonBtn_Click(object sender, EventArgs e)
+        {
+            
+            PictureBox pb2 = new PictureBox();
+            pb2.ImageLocation = "https://ustaliy.ru/wp-content/uploads/2021/11/1-741.jpg";
+            pb2.SizeMode = PictureBoxSizeMode.AutoSize;
+            this.Controls.Add(pb2);
+        }
+
+        private void BakuBtn_Click(object sender, EventArgs e)
+        {
+            PictureBox pb1 = new PictureBox();
+            pb1.ImageLocation = "https://i.pinimg.com/originals/51/9d/47/519d4750ebfd7df5fa7788fb0850621e.jpg";
+            pb1.SizeMode = PictureBoxSizeMode.AutoSize;
+            this.Controls.Add(pb1);
+        }
+    }
+}
